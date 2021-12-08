9d860d02f41c4cb8159bf1b38c359bcb1296c791
Janette <janette.castillo91@yahoo.com>
2021-12-08 00:38:39 -0600

    new files in tests folder

---
+++ _test_/Employee.test.js
@@ -0,0 +1,20 @@
+const Employee = require("../lib/employee");
+const newEmployee = new Employee("John Smith", 123, "johnsmith@johnsmith.com");
+
+describe('Employee', () => {
+    it('displays name as John Smith', () => {
+        expect(newEmployee.name).toBe("John Smith")
+    });
+
+    it('displays id as 123', () => {
+        expect(newEmployee.id).toBe(123)
+    });
+
+    it('displays email as johnsmith@johnsmith.com', () => {
+        expect(newEmployee.email).toBe("johnsmith@johnsmith.com")
+    });
+
+    it('displays role as newEmployee', () => {
+        expect(newEmployee.getRole()).toBe("Employee")
+    });
+});
