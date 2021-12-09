718b4b777bc9e21a274a5d7d1b157f8c3d644c19
Janette <janette.castillo91@yahoo.com>
2021-12-08 21:25:38 -0600

    debugging

--- _test_/Manager.test.js
+++ _test_/Manager.test.js
@@ -1,9 +1,9 @@
 const Manager = require('../lib/manager')
-const newEmployee = new Manager("John Smith", 123, "johnsmith@johnsmith.com", 2323);
+const newEmployee = new Manager("John Smith", 123, "johnsmith@johnsmith.com", 123);
 
 describe('Manager', () => {
-    it('displays office number as 2323', () => {
-        expect(newEmployee.officeNumber).toBe(2323)
+    it('displays office number as 123', () => {
+        expect(newEmployee.officeNumber).toBe(123)
     });
 
     it('displays role as manager', () => {
